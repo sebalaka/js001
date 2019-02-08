b61036524f9aad76160a1864292ff627fdd3672f
sebalaka <sebalaka@gmail.com>
2019-02-08 14:18:16 +0100

    Update 1.js
    niepotrzebne

--- 1.js
+++ 1.js
@@ -1,7 +1,7 @@
-for (var i = 0; i < 5; i++){
-    (function(x){
-        setTimeout(function(){
-          console.log(x)
-        }, i * 100)
-    })(i)
-}
+// for (var i = 0; i < 5; i++){
+//     (function(x){
+//         setTimeout(function(){
+//           console.log(x)
+//         }, i * 100)
+//     })(i)
+// }
